a378904c01adcc469463123a15a3154d93b33765
Yanislau <y.tylets@freshlimesoft.com>
2022-11-26 16:16:00 +0100

    add pol-sad result

--- src/projectSlice.js
+++ src/projectSlice.js
@@ -601,7 +601,7 @@ export const soccerScore = createSlice({
                 "away_team_id": "10",
                 "finished": "FALSE",
                 "group": "C",
-                "home_score": 0,
+                "home_score": 2,
                 "home_scorers": [
                     "null"
                 ],
