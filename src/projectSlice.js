9a17201ac2cc3990b71159173c62f69964c94fa3
Newshinsky <Swipe_28@tut.by>
2022-12-11 05:20:09 +0300

    update match result

--- src/projectSlice.js
+++ src/projectSlice.js
@@ -1635,9 +1635,9 @@ export const soccerScore = createSlice({
                     "null"
                 ],
                 "away_team_id": "27",
-                "finished": "FALSE",
+                "finished": "TRUE",
                 "group": "QR",
-                "home_score": 0,
+                "home_score": 1,
                 "home_scorers": [
                     "null"
                 ],
@@ -1647,7 +1647,7 @@ export const soccerScore = createSlice({
                 "matchday": "19",
                 "persian_date": "1400-09-19 18:30",
                 "stadium_id": "1",
-                "time_elapsed": "notstarted",
+                "time_elapsed": "finished",
                 "type": "QR",
                 "home_team_fa": "مراکش",
                 "away_team_fa": "پرتغال",
@@ -1658,14 +1658,14 @@ export const soccerScore = createSlice({
             },
             {
                 "_id": "6389265e0eab2168b945af3d",
-                "away_score": 0,
+                "away_score": 1,
                 "away_scorers": [
                     "null"
                 ],
                 "away_team_id": "5",
-                "finished": "FALSE",
+                "finished": "TRUE",
                 "group": "QR",
-                "home_score": 0,
+                "home_score": 2,
                 "home_scorers": [
                     "null"
                 ],
@@ -1675,7 +1675,7 @@ export const soccerScore = createSlice({
                 "matchday": "19",
                 "persian_date": "1400-09-19 22:30",
                 "stadium_id": "1",
-                "time_elapsed": "notstarted",
+                "time_elapsed": "finished",
                 "type": "QR",
                 "home_team_fa": "فرانسه",
                 "away_team_fa": "انگلستان",
@@ -1690,14 +1690,14 @@ export const soccerScore = createSlice({
                 "away_scorers": [
                     "null"
                 ],
-                "away_team_id": "0",
+                "away_team_id": "18",
                 "finished": "FALSE",
                 "group": "semi",
                 "home_score": 0,
                 "home_scorers": [
                     "null"
                 ],
-                "home_team_id": "0",
+                "home_team_id": "9",
                 "id": "60",
                 "local_date": "12/13/2022 22:00",
                 "matchday": "20",
@@ -1705,12 +1705,12 @@ export const soccerScore = createSlice({
                 "stadium_id": "1",
                 "time_elapsed": "notstarted",
                 "type": "semi",
-                "home_team_fa": "--",
-                "away_team_fa": "--",
-                "home_team_en": "--",
-                "away_team_en": "--",
-                "home_flag": "",
-                "away_flag": ""
+                "home_team_fa": "آرژانتین",
+                "away_team_fa": "کرواسی",
+                "home_team_en": "Argentina",
+                "away_team_en": "Croatia",
+                "home_flag": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Flag_of_Argentina.svg/125px-Flag_of_Argentina.svg.png",
+                "away_flag": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Flag_of_Croatia.svg/125px-Flag_of_Croatia.svg.png"
             },
             {
                 "_id": "6389265e0eab2168b945af3f",
@@ -1718,14 +1718,14 @@ export const soccerScore = createSlice({
                 "away_scorers": [
                     "null"
                 ],
-                "away_team_id": "0",
+                "away_team_id": "17",
                 "finished": "FALSE",
                 "group": "semi",
                 "home_score": 0,
                 "home_scorers": [
                     "null"
                 ],
-                "home_team_id": "0",
+                "home_team_id": "15",
                 "id": "61",
                 "local_date": "12/14/2022 22:00",
                 "matchday": "21",
@@ -1733,12 +1733,12 @@ export const soccerScore = createSlice({
                 "stadium_id": "1",
                 "time_elapsed": "notstarted",
                 "type": "semi",
-                "home_team_fa": "--",
-                "away_team_fa": "--",
-                "home_team_en": "--",
-                "away_team_en": "--",
-                "home_flag": "",
-                "away_flag": ""
+                "home_team_fa": "فرانسه",
+                "away_team_fa": "مراکش",
+                "home_team_en": "France",
+                "away_team_en": "Morocco",
+                "home_flag": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Flag_of_France_%281794%E2%80%931815%2C_1830%E2%80%931974%2C_2020%E2%80%93present%29.svg/125px-Flag_of_France_%281794%E2%80%931815%2C_1830%E2%80%931974%2C_2020%E2%80%93present%29.svg.png",
+                "away_flag": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Flag_of_Morocco.svg/125px-Flag_of_Morocco.svg.png"
             },
             {
                 "_id": "6389265e0eab2168b945af40",
